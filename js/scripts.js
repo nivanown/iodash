dd1fe7c37fb314b6045488ad97318a60176dbe0d
Nikita <32540777+nivanown@users.noreply.github.com>
2023-02-20 07:25:32 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+﻿/*- info-panel__item_add-button -*/
+let buttons = document.querySelectorAll(".like");
+for(let i = 0; i < buttons.length; i++) {
+  buttons.item(i).addEventListener("click", doClickButton);
+}
+
+function doClickButton(e) {
+  toggleButton(e.target);
+}
+
+function toggleButton(button) {
+  button.classList.toggle('selected');
+}
